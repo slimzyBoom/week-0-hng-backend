17b6b9c2ef283d7c8a4dfc664e138f14370a5cd9
That_Tech_Guy <169068459+slimzyBoom@users.noreply.github.com>
2025-10-16 17:14:42 +0100

    Pushed to repo

---
+++ index.js
@@ -0,0 +1,39 @@
+import "dotenv/config.js";
+import express from "express";
+import cors from "cors";
+import { rateLimit } from "express-rate-limit";
+import morgan from "morgan";
+import axios from "axios";
+const app = express();
+const PORT = process.env.PORT || 3900;
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
+
+const profile = {
+  email: process.env.USER_EMAIL,
+  name: process.env.USER_NAME,
+  stack: process.env.USER_STACK,
+};
+
+const limiter = rateLimit({
+  windowMs: 1000 * 60 * 15,
+  limit: 100,
+  message: { status: "error", message: "Too many request please try again" },
+});
+
+app.get("/me", limiter, async (req, res) => {
+  const response = await axios.get("https://catfact.ninja/fact", {
+    timeout: 1000 * 5,
+  });
+  
+  res.json({
+    status: "success",
+    user: { ...profile },
+    timestamp: Date.now(),
+    fact: response.data.fact || "Cats are fun to play with",
+  });
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
